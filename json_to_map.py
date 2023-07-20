24cb4abc8a197dacefba978c4389084356868590
vboxuser <vboxuser@newubuntu.myguest.virtualbox.org>
2023-07-19 23:02:40 -0500

    colcon build and other fixes

--- json_to_map.py
+++ json_to_map.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import os
 import json
 import yaml
 import subprocess
@@ -10,16 +13,21 @@ def convert_json_to_yaml(json_file, yaml_file):
 
 def save_map_as_pgm_and_yaml(yaml_file, map_name):
     # Save the YAML data as a .yaml file
-    with open(f"{map_name}.yaml", 'w') as outfile:
+    with open("{}.yaml".format(map_name), 'w') as outfile:
         outfile.write(yaml_file)
 
     # Call the map_saver tool from ROS 2 using subprocess
-    cmd = ["ros2", "run", "nav2_map_server", "map_saver", "-f", map_name]
+    cmd = ["/opt/ros/galactic/bin/ros2", "run", "nav2_map_server", "map_saver", "-f", map_name] # ros2 run nav2_map_server map_saver -f <map_name>
     subprocess.run(cmd)
 
 if __name__ == "__main__":
+    changepath = True
+    ros_path = "" if changepath else "/opt/ros/galactic/bin"
+    os.environ["PATH"] = ros_path + os.pathsep + os.environ["PATH"]
+    os.environ["PYTHONPATH"] = ros_path + os.pathsep + os.environ.get("PYTHONPATH","")
+
     # Specify the path to your input JSON file
-    json_file_path = 'input.json' # likely use argparse to get a file, but also provide a default
+    json_file_path = 'make_map.json' # likely use argparse to get a file, but also provide a default
 
     # Specify the desired name for your map
     map_name = 'my_map' # this works, maybe we can change it to the json's file name?
